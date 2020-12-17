aac15154d679cf553a9568803569eda5f810cdd9
knee-rel <nirelmarieibarra@gmail.com>
2020-12-17 22:38:41 +0800

    put initial content for the blank pages

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <div>
+            I am the about Page
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/products.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Products = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <div>
+            I am the Products Page
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Products
